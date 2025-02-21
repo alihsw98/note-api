062ef32451c684947ede6acfaadbe46a85c4e8eb
ali.hayder <ali.hayder@alqaseh.com>
2025-02-22 00:14:45 +0300

    add ping

--- server.js
+++ server.js
@@ -159,6 +159,6 @@ app.delete("/deleteNote/:id", authMiddleware, async (req, res) => {
 
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
